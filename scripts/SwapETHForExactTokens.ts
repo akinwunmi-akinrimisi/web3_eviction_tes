3ca8f1d707b82aef9bbd384f22d3ce28525e44a2
akinwunmi-akinrimisi <akinolaakinrimisi@gmail.com>
2024-09-17 17:47:00 +0100

    addd

--- scripts/SwapETHForExactTokens.ts
+++ scripts/SwapETHForExactTokens.ts
@@ -2,8 +2,8 @@ import { ethers } from "hardhat";
 const helpers = require("@nomicfoundation/hardhat-network-helpers");
 
 const main = async () => {
-  const DAIAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
-  const WETHAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+  const daiAddr = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+  const wethAddr = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
   const UNIRouterAddress = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
   const ETHHolder = "0xf584f8728b874a6a5c7a8d4d387c9aae9172d621";
 
@@ -12,9 +12,9 @@ const main = async () => {
 
   // This is now the exact amount of DAI you want to receive
   const amountOut = ethers.parseUnits("18", 18);
-  const path = [WETHAddress, DAIAddress];
+  const path = [wethAddr, daiAddr];
 
-  const DAI = await ethers.getContractAt("IERC20", DAIAddress);
+  const DAI = await ethers.getContractAt("IERC20", daiAddr);
   const ROUTER = await ethers.getContractAt(
     "IUniswapV2Router01",
     UNIRouterAddress,
